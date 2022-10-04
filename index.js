724cd5e5502e979d201a06472ab573f8705c48f7
Santiago Santamaria <santiago.santamaria@gmail.com>
2022-10-04 18:37:22 -0300

    basic app settings

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-//index
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+// accept json in post request
+app.use(express.json());
+
+app.get('/hello', function(req,res) {
+    return res.json("Hello World");
+});
+
+
+app.listen(5555);
