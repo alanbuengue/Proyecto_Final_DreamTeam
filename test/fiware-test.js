f647dd6ee1c4f0171d986ca1ae32ad3d04ef29b7
Santiago <santiago.santamaria@gmail.com>
2022-10-27 16:15:11 -0300

    fixes tests

--- test/fiware-test.js
+++ test/fiware-test.js
@@ -10,81 +10,81 @@ const { assert } = chai;
 
 describe('Orion Tests', function () {
     
-    // it('Shoul Get Orion Version', function (done) {
-    //     axios({
-    //         method: 'get',
-    //         url: 'http://localhost/orion',
-    //     }).then(res => {
-    //         assert.equal(res.status,200);
-    //         done();
-    //     }).catch(err => {
-    //         done(err);
-    //     });
-    // });
+    it('Shoul Get Orion Version', function (done) {
+        axios({
+            method: 'get',
+            url: 'http://localhost/orion',
+        }).then(res => {
+            assert.equal(res.status,200);
+            done();
+        }).catch(err => {
+            done(err);
+        });
+    });
 
-    // it('Should Get Orion Entities', function (done) {
-    //     axios({
-    //         method: 'get',
-    //         url: 'http://localhost/orion/entities',
-    //     }).then(res => {
-    //         assert.equal(res.status,200);
-    //         done();
-    //     }).catch(err => {
-    //         done(err);
-    //     });
-    // });
+    it('Should Get Orion Entities', function (done) {
+        axios({
+            method: 'get',
+            url: 'http://localhost/orion/entities',
+        }).then(res => {
+            assert.equal(res.status,200);
+            done();
+        }).catch(err => {
+            done(err);
+        });
+    });
 
-    // post ambiente entity
-    // it('Should Create Orion Ambiente Entity', function (done) {
+    post ambiente entity
+    it('Should Create Orion Ambiente Entity', function (done) {
         
-    //     axios({
-    //         method: 'post',
-    //         url: 'http://localhost/orion/entities',
+        axios({
+            method: 'post',
+            url: 'http://localhost/orion/entities',
             
-    //         data: {
-    //             'id': 'urn:ngsi-ld:Device:919',
-    //             'type': 'MyNewType',
-    //             'description': {
-    //                 'value': 'My entity description',
-    //                 'type': 'Text',
-    //             },
-    //             'temperature': {
-    //                 'type': 'float',
-    //                 'value': '99.0'
-    //             },
-    //             'humidity': {
-    //                 'type': 'float',
-    //                 'value': '99.0'
-    //             },
-    //             'acidity': {
-    //                 'type': 'float',
-    //                 'value': '99.0'
-    //             }    
-    //         }
-    //     }).then(res => {
-    //         assert.equal(res.status,201);
-    //         done();
-    //     }).catch(err => {
-    //         done(err);
-    //     });
-    // });
+            data: {
+                'id': 'urn:ngsi-ld:Device:919',
+                'type': 'MyNewType',
+                'description': {
+                    'value': 'My entity description',
+                    'type': 'Text',
+                },
+                'temperature': {
+                    'type': 'float',
+                    'value': '99.0'
+                },
+                'humidity': {
+                    'type': 'float',
+                    'value': '99.0'
+                },
+                'acidity': {
+                    'type': 'float',
+                    'value': '99.0'
+                }    
+            }
+        }).then(res => {
+            assert.equal(res.status,201);
+            done();
+        }).catch(err => {
+            done(err);
+        });
+    });
 
 
-    // // delete entity
-    // it('Should Delete Orion Entity', function (done) {
-    //     axios({
-    //         method: 'delete',
-    //         url: 'http://localhost/orion/entities',
-    //         data: {
-    //             'id': 'urn:ngsi-ld:Device:919',
-    //         }
-    //     }).then(res => {
-    //         assert.equal(res.status,204);
-    //         done();
-    //     }).catch(err => {
-    //         done(err);
-    //     });
-    // });
+    // delete entity
+    it('Should Delete Orion Entity', function (done) {
+        axios({
+            method: 'delete',
+            url: 'http://localhost/orion/entities',
+            data: {
+                'id': 'urn:ngsi-ld:Device:919',
+            }
+        }).then(res => {
+            assert.equal(res.status,204);
+            done();
+        }).catch(err => {
+            done(err);
+        });
+    });
 
     // IOT DEVICE TESTS
     
@@ -111,163 +111,163 @@ describe('Orion Tests', function () {
     });
 
 
-    // // Create iot service
-    // it ('Should Create A IOT Service', function (done) {
-    //     axios({
-    //         method: 'post',
-    //         url: 'http://localhost/iot/services',
-    //         data: {
-    //             'services': [
-    //                 {
-    //                   'apikey': 'test1234',
-    //                     'cbroker': 'http://46.17.108.45:1026',
-    //                     'entity_type': 'Ambiente',
-    //                     'resource': '/iot/json',
-    //                 }],
-    //             'method': 'post',
-    //             'configUrl': 'http://46.17.108.45:4041/iot/services',
-    //             'entityType': 'Ambiente',
-    //             'resource': '/iot/json',
-    //             'headers': { 
-    //                 'Fiware-Service': 'testService3', 
-    //                 'Fiware-ServicePath': '/', 
-    //                 'Content-Type': 'application/json'
-    //             }
-    //         }
+    // Create iot service
+    it ('Should Create A IOT Service', function (done) {
+        axios({
+            method: 'post',
+            url: 'http://localhost/iot/services',
+            data: {
+                'services': [
+                    {
+                      'apikey': 'test1234',
+                        'cbroker': 'http://46.17.108.45:1026',
+                        'entity_type': 'Ambiente',
+                        'resource': '/iot/json',
+                    }],
+                'method': 'post',
+                'configUrl': 'http://46.17.108.45:4041/iot/services',
+                'entityType': 'Ambiente',
+                'resource': '/iot/json',
+                'headers': { 
+                    'Fiware-Service': 'testService3', 
+                    'Fiware-ServicePath': '/', 
+                    'Content-Type': 'application/json'
+                }
+            }
         
-    //     }).then(res => {
-    //         assert.equal(res.status,201);
-    //         done();
-    //     }).catch(err => {
-    //         done(err);
-    //     });
-    // });        
+        }).then(res => {
+            assert.equal(res.status,201);
+            done();
+        }).catch(err => {
+            done(err);
+        });
+    });        
 
-    // // get iot service
-    // it('Should Get Iot Created Service', function (done) {
-    //     axios({
-    //         method: 'get',
-    //         url: 'http://localhost/iot/services',
-    //         data: {
-    //             'headers': { 
-    //                 'Fiware-Service': 'testService3', 
-    //                 'Fiware-ServicePath': '/', 
-    //                 'Content-Type': 'application/json'
-    //             }
-    //         }
-    //     }).then(res => {
-    //         assert.equal(res.status,200);
-    //         done();
-    //     }).catch(err => {
-    //         done(err);
-    //     });
-    // });
+    // get iot service
+    it('Should Get Iot Created Service', function (done) {
+        axios({
+            method: 'get',
+            url: 'http://localhost/iot/services',
+            data: {
+                'headers': { 
+                    'Fiware-Service': 'testService3', 
+                    'Fiware-ServicePath': '/', 
+                    'Content-Type': 'application/json'
+                }
+            }
+        }).then(res => {
+            assert.equal(res.status,200);
+            done();
+        }).catch(err => {
+            done(err);
+        });
+    });
 
 
-    // // SENSOR DATA TESTS
+    // SENSOR DATA TESTS
 
-    // // get sensor data
-    // it('Should Get Sensor Data', function (done) {
-    //     axios({
-    //         method: 'get',
-    //         url: 'http://localhost/iot',
-    //         data: {
-    //             'headers': {
-    //                 'Accept': '*/*',
-    //                 'Connection': 'keep-alive',
-    //                 'Fiware-Service': 'sensor',
-    //                 'Fiware-ServicePath': '/',
-    //             },
-    //             'entityId': 'ambiente:001',
-    //             'entityType': 'Ambiente',
-    //         },
-    //     }).then(res => {
-    //         assert.equal(res.status,200);
-    //         done();
-    //     }).catch(err => {
-    //         done(err);
-    //     });
-    // });
+    // get sensor data
+    it('Should Get Sensor Data', function (done) {
+        axios({
+            method: 'get',
+            url: 'http://localhost/iot',
+            data: {
+                'headers': {
+                    'Accept': '*/*',
+                    'Connection': 'keep-alive',
+                    'Fiware-Service': 'sensor',
+                    'Fiware-ServicePath': '/',
+                },
+                'entityId': 'ambiente:001',
+                'entityType': 'Ambiente',
+            },
+        }).then(res => {
+            assert.equal(res.status,200);
+            done();
+        }).catch(err => {
+            done(err);
+        });
+    });
 
-    // // send sensor data
-    // it('Should Send Sensor Data', function (done) {
-    //     axios({
-    //         method: 'post',
-    //         url: 'http://localhost/iot',
-    //         data: {
-    //             'device_id': 'ambiente-sensor:001',
-    //             'temperature': '23.0',
-    //             'humidity': '24.0',
-    //             'acidity': '7.0',
-    //             'headers': { 
-    //                 'Fiware-Service': 'sensor', 
-    //                 'Fiware-ServicePath': '/',
-    //                 'X-Auth-Token': 'asd12345',
-    //                 'Content-Type': 'application/json'
-    //             }
-    //         }
-    //     }).then(res => {
-    //         assert.equal(res.status,201);
-    //         done();
-    //     }).catch(err => {
-    //         done(err);
-    //     });
-    // });
+    // send sensor data
+    it('Should Send Sensor Data', function (done) {
+        axios({
+            method: 'post',
+            url: 'http://localhost/iot',
+            data: {
+                'device_id': 'ambiente-sensor:001',
+                'temperature': '23.0',
+                'humidity': '24.0',
+                'acidity': '7.0',
+                'headers': { 
+                    'Fiware-Service': 'sensor', 
+                    'Fiware-ServicePath': '/',
+                    'X-Auth-Token': 'asd12345',
+                    'Content-Type': 'application/json'
+                }
+            }
+        }).then(res => {
+            assert.equal(res.status,201);
+            done();
+        }).catch(err => {
+            done(err);
+        });
+    });
 
-    // // create sensor device
-    // it('Should Create A Sensor Device', function (done) {
-    //     axios({
-    //         method: 'post',
-    //         url: 'http://localhost/iot/devices',
+    // create sensor device
+    it('Should Create A Sensor Device', function (done) {
+        axios({
+            method: 'post',
+            url: 'http://localhost/iot/devices',
             
-    //         data: {
-    //             'devices': {
-    //                     'device_id': 'ambiente-sensor:007',
-    //                     'entity_name': 'ambiente:007',
-    //                     'entity_type': 'Ambiente',
-    //                     'timezone': 'America/Buenos_Aires',
-    //                     'attributes': [
-    //                         {
-    //                             'object_id': 'temperature',
-    //                             'name': 'temperature',
-    //                             'type': 'Float'
-    //                         },
-    //                         {
-    //                             'object_id': 'humidity',
-    //                             'name': 'humidity',
-    //                             'type': 'Float'
-    //                         },
-    //                         {
-    //                             'object_id': 'acidity',
-    //                             'name': 'acidity',
-    //                             'type': 'Float'
-    //                         }
-    //                     ],
-    //                     'static_attributes': [
-    //                         {
-    //                             'name': 'refStore',
-    //                             'type': 'Relationship',
-    //                             'value': 'urn:ngsi-ld:Store:001'
-    //                         }
-    //                     ]
-    //             },
-    //             'headers': {
-    //                 'Fiware-Service': 'sensor',
-    //                 'Fiware-ServicePath': '/',
-    //                 'X-Auth-Token': '666ppp', 
-    //                 'Content-Type': 'application/json'
-    //             }
-    //         }
+            data: {
+                'devices': {
+                        'device_id': 'ambiente-sensor:007',
+                        'entity_name': 'ambiente:007',
+                        'entity_type': 'Ambiente',
+                        'timezone': 'America/Buenos_Aires',
+                        'attributes': [
+                            {
+                                'object_id': 'temperature',
+                                'name': 'temperature',
+                                'type': 'Float'
+                            },
+                            {
+                                'object_id': 'humidity',
+                                'name': 'humidity',
+                                'type': 'Float'
+                            },
+                            {
+                                'object_id': 'acidity',
+                                'name': 'acidity',
+                                'type': 'Float'
+                            }
+                        ],
+                        'static_attributes': [
+                            {
+                                'name': 'refStore',
+                                'type': 'Relationship',
+                                'value': 'urn:ngsi-ld:Store:001'
+                            }
+                        ]
+                },
+                'headers': {
+                    'Fiware-Service': 'sensor',
+                    'Fiware-ServicePath': '/',
+                    'X-Auth-Token': '666ppp', 
+                    'Content-Type': 'application/json'
+                }
+            }
         
             
-    //     }).then(res => {
-    //         assert.equal(res.status,201);
+        }).then(res => {
+            assert.equal(res.status,201);
             
-    //         done();
-    //     }).catch(err => {
-    //         done(err);
-    //     });
-    // });
+            done();
+        }).catch(err => {
+            done(err);
+        });
+    });
     
 
 });
