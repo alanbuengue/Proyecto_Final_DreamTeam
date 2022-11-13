4ec25dfdc48ab53892910fa3d44467819c312b65
Alex Teper <ateper@fi.uba.ar>
2022-11-13 18:51:40 -0300

    fix applisten

--- index.js
+++ index.js
@@ -1325,5 +1325,3 @@ app.get('/user/crop', async function (req, res) {
 app.listen(process.env.PORT || 5001, () => console.log("run server PORT " + process.env.PORT))
 //app.listen(80);
 
-app.listen(process.env.PORT || 5001, () => console.log("run server PORT " + process.env.PORT))
-//app.listen(80);
